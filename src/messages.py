fe23d37c46837d5282ace1b71ac7b1c0096f3ccd
Guriev Nicholas <guriev-ns@ya.ru>
2015-05-04 15:32:44 +0300

    Теперь в беседах отображаются имена отправителей

--- src/messages.py
+++ src/messages.py
@@ -4,10 +4,25 @@ class Messages(FeatureInterface):
 	"""Отвечает за работу экрана с сообщениями из конкретного диалога"""
 
 	def get_data(self, chat_id=0, user_id=0):
-		return self.common.vkapi.messages.getHistory(chat_id=chat_id, user_id=user_id).items
+		messages = self.common.vkapi.messages.getHistory(chat_id=chat_id, user_id=user_id).items
+
+		# если в диалоге есть сообщения и этот диалог является беседой
+		if len(messages) and 'chat_id' in messages[0]:
+			for msg in messages:
+				self.common.users.add(msg.from_id)
+			self.common.users.fill_all("online")
+
+		return messages
 
 	def render(self, messages):
+		last_msg_author = None
 		for msg in messages[::-1]:
+			# печаем имя пользователя, если мы в беседе и это входящее сообщение
+			# дважды подряд имя пользователя не печатаем
+			if not msg.out and 'chat_id' in msg and last_msg_author != msg.from_id:
+				last_msg_author = msg.from_id
+				self.common.console.write(self.common.users[msg.from_id].header())
+
 			align = '>' if msg.out else '<'
 			for line in msg.body.split('\n'):
 				self.common.console.write(" "*13, "{0:{1}80}".format(line, align))

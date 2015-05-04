9903b576c6e07c7241a440842ae68c81f492576f
Guriev Nicholas <guriev-ns@ya.ru>
2015-05-04 10:07:48 +0300

    Библиотека argparse

--- src/vk-cli.py
+++ src/vk-cli.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python3
-import sys
+import argparse
 from dialogs import Dialogs
 from hub import Hub
 from messages import Messages
 from sender import Sender
 
+def get_args():
+	parser = argparse.ArgumentParser()
+	subparsers = parser.add_subparsers(dest="action")
+
+	dialogs_parser = subparsers.add_parser("dialogs")
+
+	show_parser = subparsers.add_parser("show")
+	show_parser.add_argument("--id", type=int)
+
+	send_parser = subparsers.add_parser("send")
+
+	return parser.parse_args()
+
 def app():
 	Hub()	# инициализируем одиночку
-	if len(sys.argv) == 1:
+	args = get_args()
+	if args.action == None or args.action == "dialogs":
 		Dialogs().call()
-	elif sys.argv[1] == "show":
-		Messages().call(user_id=sys.argv[2])
-	elif sys.argv[1] == "send":
+	elif args.action == "show":
+		Messages().call(args.id)
+	elif args.action == "send":
 		Sender().call()
 
 if __name__ == "__main__": app()
